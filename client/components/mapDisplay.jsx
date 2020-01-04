f0a6d7568c0f9f4b4fd742976146513497cd8efb
Kelly <kellydekitani@gmail.com>
2020-01-04 15:40:09 -0800

    added a basic search bar, form, and imageDisplay component

--- client/components/mapDisplay.jsx
+++ client/components/mapDisplay.jsx
@@ -28,6 +28,10 @@ class SimpleMap extends Component {
   }
   render() {
       return (
+        <div>
+          <div>
+            <input type = "text" placeholder="Filter marker by tag"/>
+          </div>
           <div style={{ width: '70vw' , height: '70vh'}}>
               <MapWrapped 
               googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyANq9H8F3LekACFLwylqb9dGv-Bgj2-kww'}
@@ -36,6 +40,7 @@ class SimpleMap extends Component {
               mapElement={<div style={{height: '100%'}} /> }
               />
           </div>
+        </div>   
       )
   }
 }
