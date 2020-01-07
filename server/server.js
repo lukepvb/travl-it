15ec1f78af5a85dbc0470e4a9afe0d4012498668
Taylor Rodrigues <taylor.d.rod33@gmail.com>
2020-01-07 15:51:10 -0800

    updated server config, webpack config, package.json

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 const cloudinary = require('cloudinary')
 const formData = require('express-form-data')
 
-const PORT = 8080;
+const PORT = 3000;
 
 const app = express();
 
@@ -16,22 +16,29 @@ const userController = require('./controllers/controller');
 
 
 app.use(formData.parse())
-app.use(bodyParser.json());
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 // route to get all markers and main page
-app.get('/',  (req, res) => {
-  res.status(200).sendFile(path.join(__dirname , '../index.html'));
-})
+/**
+ * handle requests for static files
+ */
+app.use('/assets', express.static(path.join(__dirname, './assets')));
+
+if (process.env.NODE_ENV === 'production') {
+  // statically serve everything in the build folder on the route '/build'
+  app.use('/build', express.static(path.join(__dirname, '../build')));
+  // serve index.html on the route '/'
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'));
+  });
+}
+
+
 
 app.get('/api', userController.getMarkers, (req, res) => {
-  res.status(200).json({markersList: res.locals.markersList})
+  res.status(200).json({ markersList: res.locals.markersList })
 })
-
-app.get('/build/bundle.js', (req, res) => {
-  console.log('inside / build-get') 
-  res.sendFile(path.resolve(__dirname, '../build/bundle.js'));
-});
-
 // route to create a marker on first click
 app.post('/addMarker', userController.addMarker, (req, res) => {
   res.status(200).send("Marker created!");
@@ -43,7 +50,7 @@ app.post('/addImage', userController.addImage, (req, res) => {
 
 // route to update marker when you submit form
 app.patch('/updateMarker', userController.updateMarker, userController.getOneMarker, (req, res) => {
-  res.status(200).json({ updatedMarker: res.locals.oneMarker});
+  res.status(200).json({ updatedMarker: res.locals.oneMarker });
 })
 
 //this is a test to see if the query to the DB works - had to use another route because of the original '/' get request that serves the index.html
@@ -51,18 +58,25 @@ app.get('/getusers', userController.getUser, (req, res) => {
   res.status(200).send('this works man!');
 })
 
+app.get('/', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, '../client/index.html'));
+})
 
-app.use('*', (req,res) => {
-    res.status(404).send('Not Found');
-  });
-
-
-
+app.use('*', (req, res) => {
+  res.sendStatus(404);
+});
 
 app.use((err, req, res, next) => {
-    console.log(err);
-    res.status(500).send('Internal Server Error');
-  });
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
 
+app.listen(PORT, () => { console.log(`Listening on port ${PORT}...`); });
 
-app.listen(PORT, ()=>{ console.log(`Listening on port ${PORT}...`); });
+module.exports = app;
