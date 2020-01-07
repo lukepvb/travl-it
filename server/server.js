f877ab0cddf8975a7d5b10e7863797c3d96ea4f9
Taylor Rodrigues <taylor.d.rod33@gmail.com>
2020-01-07 15:54:26 -0800

    remove bodyparser package

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@
 
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
 const cloudinary = require('cloudinary')
 const formData = require('express-form-data')
 
