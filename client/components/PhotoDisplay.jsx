b43d0c1228bceddcbb6beb7433dd8a3dfac04d23
Dave Franz <davidlawrencefranz@gmail.com>
2020-01-08 14:34:20 -0800

    created skeleton component files for a trips stop:DestinationDisplay, PhotoDisplay, and DescriptionDisplay

---
+++ client/components/PhotoDisplay.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PhotoDisplay = props => {
+
+  return (
+    <div>
+      <h3>Photo Display</h3>
+      <p>A photo or image gallery</p>
+    </div>
+  )
+}
+
+export default PhotoDisplay;
